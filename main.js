f7ae52fd7e26e36ede76bf44436c94f04936147a
harrysonhall <114112064+harrysonhall@users.noreply.github.com>
2023-03-17 20:00:19 -0400

    Setup the basic structure of the header and the footer of the landing page.

---
+++ main.js
@@ -0,0 +1,25 @@
+// const sub_header_text = document.querySelectorAll(".sub-header-text");
+// const line = document.querySelector('#line')
+// const stylesheet = document.styleSheets[0].cssRules
+// console.log(stylesheet)
+
+// for(const text of sub_header_text) {
+// 	text.addEventListener('mouseover', e => {
+// 		if(e.target.lastElementChild) {
+// 			// console.log(e.target.lastElementChild.style)
+// 			e.target.lastElementChild.style.setProperty("align-self", "right");
+// 			e.target.lastElementChild.style.setProperty("width", "50%");
+// 			// e.target.lastElementChild.style.setProperty('width', '100%')
+// 		}
+// 	})
+// }
+
+// for(const text of sub_header_text) {
+// 	text.addEventListener('mouseout', e => {
+// 		if(e.target.lastElementChild) {
+// 			e.target.lastElementChild.style.setProperty("align-self", "left");
+// 			// e.target.lastElementChild.style.setProperty("width", "0%");
+// 			// console.log('out')
+// 		}
+// 	})
+// }
